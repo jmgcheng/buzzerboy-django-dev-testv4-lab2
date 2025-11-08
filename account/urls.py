a62677897ecf3430c2c7b6125b6c0d48843330f5
jcheng-servicesolutions <jcheng@servicesolutions.biz>
2025-11-08 09:00:30 +0800

    Include .po translation files for all apps

--- account/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from account.views import switch_profile
-
-
-urlpatterns = [
-    path('profiles/<int:pk>/switch/', switch_profile, name='switch-profile'),
-]
